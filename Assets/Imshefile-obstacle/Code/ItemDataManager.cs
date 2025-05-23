45389c0d32a92c6b7b60e464afd8186853b50d3d
RohSeongmin <smroh0509@gmail.com>
2025-05-23 16:47:26 +0900

    refactor Add Coffee Data
    Add coffee datas

--- Assets/Imshefile-obstacle/Code/ItemDataManager.cs
+++ Assets/Imshefile-obstacle/Code/ItemDataManager.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 // getAmount(string item_name): returns Damage of Obstacles (Negative) or Amount of Recovery items (Positive)
 //                              and it requires exact NAMES of the Sprite
 //                              
-//                              Item Lists |        Obstacles - barricade, bugs, do, puddle
-//                                         |   Recovery items - coffee
+//                              Item Lists |        Obstacles - barricade, bugs, do, pickpocket, puddle
+//                                         |   Recovery items - vmcoffee, cvcoffee, pdcoffee, espresso, dclatte, coldbrew
 public class ItemDataManager : MonoBehaviour
 {
     public int getAmount(string item_name)
@@ -20,10 +20,22 @@ public class ItemDataManager : MonoBehaviour
                 return -1;
             case "do":
                 return -1;
+            case "pickpocket":
+                return -1;
             case "puddle":
                 return -1;
             // recovery
-            case "coffee":
+            case "vmcoffee":
+                return +1;
+            case "cvcoffe":
+                return +1;
+            case "pdcoffee":
+                return +1;
+            case "espresso":
+                return +1;
+            case "dclatte":
+                return +1;
+            case "coldbrew":
                 return +1;
         }
         return 0;
