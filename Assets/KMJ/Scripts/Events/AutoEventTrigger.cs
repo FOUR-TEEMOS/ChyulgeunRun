2ba54310dd538ac344712d36029605c1eb132f6b
tlsrms <kpipi1219@naver.com>
2025-06-06 21:14:43 +0900

    [Merge] Kim & Singeun 완료
    완료

--- Assets/KMJ/Scripts/Events/AutoEventTrigger.cs
+++ Assets/KMJ/Scripts/Events/AutoEventTrigger.cs
@@ -5,14 +5,7 @@ public class AutoEventTrigger : MonoBehaviour
 {
     void Start()
     {
-        // 최초 1회만 PlayerPrefs 전체 초기화
-        if (!PlayerPrefs.HasKey("Initialized"))
-        {
-            PlayerPrefs.DeleteAll();  // 전체 리셋
-            PlayerPrefs.SetInt("Initialized", 1);  // 초기화 완료 마크 저장
-            PlayerPrefs.Save();
-            Debug.Log("PlayerPrefs 전체 초기화 완료!");
-        }
+         //PlayerPrefs.DeleteAll();
 
         //Event1이 아직 안 했으면 EventScene1 로드
         if (!PlayerPrefs.HasKey("Event1_Done"))
